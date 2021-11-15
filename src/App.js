7f48e00097fbafd1205bc96ad35242193dd35ec1
iz590985 <you@example.com>
2021-11-15 11:00:49 -0500

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from "react-particles-js";
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
+    <>
+    <Particles
+        className="particles-canvas"
+        params={{
+          	particles: {
+          		
+          		// Number of particles
+            	number: {
+              		value: 30,
+              		density: {
+                		enable: true,
+                		value_area: 900
+              		}
+            	},
+            	// Shape of the particles
+            	shape: {
+            		type: "square",
+            		stroke: {
+            			width: 6,
+            			color: "#f9ab00"
+            		}
+            	}
+          	}
+        }}
+    />
     <Navbar />
+    <Header />
+    </>
   );
 }
 
